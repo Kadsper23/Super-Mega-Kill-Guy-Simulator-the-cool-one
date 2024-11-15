8f187e324c1705a8e82c694ccc3b67d697c88507
Kadsper23 <kadensperry7@gmail.com>
2024-11-15 13:59:13 -0700

    balls

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private bool isDragging = false;
+    public GameObject Canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas = GameObject.Find("Main Canvas");
+    }
+    public void StartDrag()
+    {
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
+        }
+    }
+}
--- Assets/scrii.cs
+++ Assets/scrii.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class scrii : MonoBehaviour
+
+
 {
     // Start is called before the first frame update
     void Start()
