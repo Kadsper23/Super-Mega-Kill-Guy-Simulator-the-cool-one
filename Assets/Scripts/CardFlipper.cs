c8c8d487758d66fca86a5c407c993028d965d0ee
Kadsper23 <kadensperry7@gmail.com>
2024-11-16 00:16:42 -0700

    w

---
+++ Assets/Scripts/CardFlipper.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardFlipper : MonoBehaviour
+{
+    public Sprite CardFront;
+    public Sprite CardBack;
+}
