494ecc8893a8e5956973630b54124377ac06474f
Kadsper23 <kadensperry7@gmail.com>
2024-11-15 17:52:14 -0700

    e

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -5,21 +5,51 @@ using UnityEngine;
 public class DragDrop : MonoBehaviour
 {
     private bool isDragging = false;
+    private GameObject startParent;
+    private Vector2 startPosition;
+    private GameObject dropZone;
     public GameObject Canvas;
+    public GameObject DropZone;
+    private bool isOverDropZone;
 
     // Start is called before the first frame update
     void Start()
     {
         Canvas = GameObject.Find("Main Canvas");
+        DropZone = GameObject.Find("DropZone");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropZone = null;
     }
     public void StartDrag()
     {
         isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
     }
 
     public void EndDrag()
     {
         isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
     }
 
     // Update is called once per frame
